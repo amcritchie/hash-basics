96c14c8883dcb519dca005db161b80df2f6f0185
Alex McRitchie <amcritchie@gmail.com>
2014-06-19 14:04:39 -0600

    Commiting Borg's address

--- 05_print_address.rb
+++ 05_print_address.rb
@@ -1,3 +1,16 @@
 require_relative "person"
 
 # Print Bjorn's address. It should read "444 Borg Lane, San Francisco, CA, 94104"
+
+temp = []
+address = []
+
+temp = BJORN_BORG["address"].to_a
+address[0] = temp[0].pop
+address[1] = temp[1].pop
+address[2] = temp[2].pop
+address[3] = temp[3].pop
+
+p address.join(", ")
+
+
