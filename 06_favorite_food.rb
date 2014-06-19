b21bce0989a5fb7d6fe2541a1de763af1f7b71c4
Alex McRitchie <amcritchie@gmail.com>
2014-06-19 14:18:22 -0600

    Commiting Borg's fav foods

--- 06_favorite_food.rb
+++ 06_favorite_food.rb
@@ -1,3 +1,6 @@
 require_relative "person"
 
 # Print Bjorn's favorite foods. It should read "Bjorn's favorite foods are sushi, hamburgers, and mexican food."
+
+p food = "#{BJORN_BORG["first_name"]}'s favorite foods are #{BJORN_BORG["favorite_foods"].join(", ")}"
+p food.insert(46, "and ")
